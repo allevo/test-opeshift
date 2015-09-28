ec4a8be5be877f8924cc430ea57bd31caee105e9
allevo <tomallevi@gmail.com>
2015-09-28 15:57:59 +0200

    Add mongodb interaction

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 'use strict';
 
 var app = require('express')();
+var MongoClient = require('mongodb').MongoClient;
 
+var url = 'mongodb://' + (process.env.MONGO_SERVICE_HOST || 'localhost') + ':27017/foo';
+
+app.get('/database', function(req, res) {
+	MongoClient.connect(url, function(err, db) {
+		console.log("Connected correctly to server", err);
+		var collection = db.collection('documents');
+		collection.insert([
+			{a : 1}, {a : 2}, {a : 3}
+		], function(err, result) {
+			console.log("Inserted 3 documents into the document collection");
+			console.log(err, result);
+			collection.find({}).toArray(function(err, docs) {
+				res.json(docs);
+				db.close();
+			});
+		});
+	});
+});
 app.get('/', function(req, res) {
 	res.json(process.env);
 });
